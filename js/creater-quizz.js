030abbd66b8c2c4e25a412ffba07850a089bc1cc
Rafael <rafinha_fern@hotmail.com>
2022-02-16 02:54:27 -0300

    iniciando js

---
+++ js/creater-quizz.js
@@ -0,0 +1,5 @@
+let quizzCreateTitle = document.getElementById('creater-input-title').value
+function firstCreaterBtn() {
+    quizzCreateTitle = document.getElementById('creater-input-title').value
+    console.log(quizzCreateTitle)
+}

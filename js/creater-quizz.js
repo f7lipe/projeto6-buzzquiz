dba2da3e02891f0465e2cf21995ba6f368545caf
Rafael <rafinha_fern@hotmail.com>
2022-02-18 04:53:05 -0300

    tela 3.2 filtro

--- js/creater-quizz.js
+++ js/creater-quizz.js
@@ -75,7 +75,7 @@ function quizzCreateValidate() {
         // se passar tudo na paz ele exibe a proxima pagina de perguntas 3.2
         quizzLayerOne.classList.add("hidden");
         quizzLayerTwo.classList.remove("hidden");
-        createrLayerTwoRenderizer()
+        createrLayerTwoRenderizer();
     }
 }
 // funcao para sair da tela de entrada invalida,
@@ -90,7 +90,7 @@ function invalidInputLayerHidden() {
 function createrLayerTwoRenderizer() {
     quizzLayerTwo.innerHTML = `<div class="creater-header creater-header-questions">
     <p>Crie suas perguntas</p>
-    </div>`
+    </div>`;
     for (i = 1; i <= quizzCreateQuestionsNumb; i++) {
         quizzLayerTwo.innerHTML += `
 <div id="questions-minimized-${i}" onclick="layerQuestionsToggle(${i})" class="questions-minimized">
@@ -117,6 +117,8 @@ function createrLayerTwoRenderizer() {
   class="creater-input"
   type="text"
   name="quizz[color]"
+  minlength="7"
+  maxlength="7" value="#"
 />
 <div class="creater-input-container-header">
   <p>Resposta Correta</p>
@@ -124,18 +126,16 @@ function createrLayerTwoRenderizer() {
 <input
   id="creater-input-questions-answer${i}"
   placeholder="Resposta correta"
-  class="creater-input"
-  type="number"
+  class="creater-input true"
+  type="text"
   name="quizz[questions]"
-  min="3"
 />
 <input
   id="creater-input-questions-image${i}"
   placeholder="URL da imagem"
-  class="creater-input"
-  type="number"
-  name="quizz[level-numbers]"
-  min="2"
+  class="creater-input true-image"
+  type="url"
+  name="quizz[image-URL]"
 />
 <div class="creater-input-container-header">
   <p>Respostas Incorretas</p>
@@ -143,87 +143,101 @@ function createrLayerTwoRenderizer() {
 <input
   id="creater-input-questions-wanswer${i}"
   placeholder="Resposta incorreta 1"
-  class="creater-input"
-  type="number"
-  name="quizz[questions]"
-  min="3"
+  class="creater-input wrong-answer"
+  type="text"
+  name="quizz[questions-wanswer]"
 />
 <input
   id="creater-input-questions-wimage${i}"
   placeholder="URL da imagem 1"
-  class="creater-input"
-  type="number"
-  name="quizz[level-numbers]"
-  min="2"
+  class="creater-input "
+  type="url"
+  name="quizz[image-URL]"
 />
 <input
   id="creater-input-questions-wanswer${i}"
   placeholder="Resposta incorreta 2"
   class="creater-input"
-  type="number"
-  name="quizz[questions]"
-  min="3"
+  type="text"
+  name="quizz[questions-wanswer]"
 />
 <input
   id="creater-input-questions-wimage${i}"
   placeholder="URL da imagem 2"
   class="creater-input"
-  type="number"
-  name="quizz[level-numbers]"
-  min="2"
+  type="url"
+  name="quizz[image-URL]"
 />
 <input
   id="creater-input-questions-wanswer${i}"
   placeholder="Resposta incorreta 3"
   class="creater-input"
-  type="number"
-  name="quizz[questions]"
-  min="3"
+  type="text"
+  name="quizz[questions-wanswer]"
 />
 <input
   id="creater-input-questions-wimage${i}"
   placeholder="URL da imagem 3"
   class="creater-input"
-  type="number"
-  name="quizz[level-numbers]"
-  min="2"
+  type="url"
+  name="quizz[image-URL]"
 />
 </div>
 `;
     }
-    const questioOneBtn = document.getElementById('questions-minimized-1');
-    const questionOne = document.getElementById('creater-class-container1');
-    questioOneBtn.classList.add('hidden')
-    questionOne.classList.remove('hidden')
-    questionOne.classList.add('visible')
+    const questioOneBtn = document.getElementById("questions-minimized-1");
+    const questionOne = document.getElementById("creater-class-container1");
+    questioOneBtn.classList.add("hidden");
+    questionOne.classList.remove("hidden");
+    questionOne.classList.add("visible");
     quizzLayerTwo.innerHTML += `<div class ="second-btn"><button onclick="secondCreaterBtn()" class="btn-creater">
         Prosseguir pra criar perguntas
       </button>
-      </div>`
-    window.scrollTo(0, 0)
+      </div>`;
+    window.scrollTo(0, 0);
 }
 
 function layerQuestionsToggle(i) {
-    let listQuestionBtn = document.querySelectorAll('.questions-minimized');
-    let listQuestion = document.querySelectorAll('.creater-input-container');
-    listQuestionBtn.forEach(questionBtn => {
-        if (questionBtn.classList.contains('hidden')) {
-            questionBtn.classList.remove('hidden')
+    let listQuestionBtn = document.querySelectorAll(".questions-minimized");
+    let listQuestion = document.querySelectorAll(".creater-input-container");
+    listQuestionBtn.forEach((questionBtn) => {
+        if (questionBtn.classList.contains("hidden")) {
+            questionBtn.classList.remove("hidden");
         }
-    })
-    listQuestion.forEach(question => {
-        if (question.classList.contains('visible')) {
-            question.classList.remove('visible')
-            question.classList.add('hidden')
+    });
+    listQuestion.forEach((question) => {
+        if (question.classList.contains("visible")) {
+            question.classList.remove("visible");
+            question.classList.add("hidden");
         }
-    })
-    let questionBtnHidden = document.getElementById(`questions-minimized-${i}`)
-    let questionVisible = document.getElementById(`creater-class-container${i}`)
-    questionBtnHidden.classList.add('hidden')
-    questionVisible.classList.remove('hidden')
-    questionVisible.classList.add('visible')
-    window.scrollTo({ top: 69, behavior: 'smooth' })
+    });
+    let questionBtnHidden = document.getElementById(`questions-minimized-${i}`);
+    let questionVisible = document.getElementById(`creater-class-container${i}`);
+    questionBtnHidden.classList.add("hidden");
+    questionVisible.classList.remove("hidden");
+    questionVisible.classList.add("visible");
+    window.scrollTo({ top: 69, behavior: "smooth" });
 }
 
-// botao de prosseguir da pagina 3.2 para a 3.3
-function secondCreaterBtn() {}
+// botao inicia a validacao da pagina 3.2
+function secondCreaterBtn() {
+    let regExpHex = new RegExp(/^[#][0-9A-F]{6}$/i);
+    let contador = 0;
+    for (i = 1; i <= quizzCreateQuestionsNumb; i++) {
+        let questionAswers = document.getElementById(
+            `creater-input-questions-answer${i}`
+        ).value;
+        let questionColor = document.getElementById(
+            `creater-input-question-color-${i}`
+        ).value;
+        let questionWrong = document.querySelectorAll('')
+        if (!regExpHex.test(questionColor)) {
+            contador++
+        }
+        if (questionAswers === "") {
+            alert(`responde ai a numero ${i}`)
+            contador++
+        }
+
+    }
+}

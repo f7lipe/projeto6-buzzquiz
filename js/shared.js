e9efc25dfb84ac563623dccc45754003c2ce28f6
Filipe Correia <f7lipe@Me.com>
2022-02-19 20:50:33 -0300

    feat: Finalização de quizz

--- js/shared.js
+++ js/shared.js
@@ -4,3 +4,9 @@ const API_REPO = 'https://mock-api.driven.com.br/api/v4/buzzquizz/'
 function comparador(){
     return Math.random() - 0.5
 }
+
+const userSession = {
+    userQuizzes : ''
+}
+
+const quizViewrDiv = document.querySelector('.quiz-viewr')
